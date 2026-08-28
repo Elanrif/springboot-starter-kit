745a954e27e41b8af41847a7afd0f3b261a05655
Elanrif <saidbacoelanrif@gmail.com>
2026-08-28 17:38:45 +0100

    fix: authentication

--- src/main/java/com/elanrif/springbootstarterkit/controller/AuthController.java
+++ src/main/java/com/elanrif/springbootstarterkit/controller/AuthController.java
@@ -42,7 +42,7 @@ public class AuthController {
             @Valid @RequestBody AuthDto.RegisterRequest request
     ) {
         log.info(
-                "POST /api/v1/auth/register - Registration attempt for email: {}",
+                "POST /api/v1/register - Registration attempt for email: {}",
                 request.email()
         );
 
@@ -54,12 +54,12 @@ public class AuthController {
                 .body(response);
     }
 
-    @PatchMapping("/me")
+    @PatchMapping("/account")
     public ResponseEntity<UserDto.Response> updateMyProfile(
             @Valid @RequestBody AuthDto.ProfileUpdateRequest request
     ) {
         log.info(
-                "PATCH /api/v1/auth/me - Updating profile for email: {}",
+                "PATCH /api/v1/auth/account - Updating profile for email: {}",
                 request.email()
         );
 
@@ -69,12 +69,12 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
-    @PatchMapping("/me/password")
+    @PatchMapping("/account/password")
     public ResponseEntity<Void> changeMyPassword(
             @Valid @RequestBody AuthDto.ChangePasswordRequest request
     ) {
         log.info(
-                "PATCH /api/v1/auth/me/password - Changing password for email: {}",
+                "PATCH /api/v1/auth/account/password - Changing password for email: {}",
                 request.email()
         );
 
@@ -83,12 +83,12 @@ public class AuthController {
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/password/reset")
+    @PatchMapping("/account/reset-password")
     public ResponseEntity<Void> resetMyPassword(
             @Valid @RequestBody AuthDto.ResetPasswordRequest request
     ) {
         log.info(
-                "PATCH /api/v1/auth/password/reset - Resetting password for email: {}",
+                "PATCH /api/v1/auth/account/reset-password - Resetting password for email: {}",
                 request.email()
         );
 
@@ -97,12 +97,12 @@ public class AuthController {
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/me")
+    @DeleteMapping("/account")
     public ResponseEntity<Void> deleteMyAccount(
             @Valid @RequestBody AuthDto.DeleteAccountRequest request
     ) {
         log.info(
-                "DELETE /api/v1/auth/me - Deleting account"
+                "DELETE /api/v1/auth/account - Deleting account"
         );
 
         authService.deleteMyAccount(request);
