61964c11ac3d88233c2b0ebc547845a09bb245d0
Elanrif <saidbacoelanrif@gmail.com>
2026-09-15 11:23:58 +0100

    feat: enable jpa auditing

--- src/main/java/com/elanrif/springbootstarterkit/SpringbootStarterKitApplication.java
+++ src/main/java/com/elanrif/springbootstarterkit/SpringbootStarterKitApplication.java
@@ -2,8 +2,10 @@ package com.elanrif.springbootstarterkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootStarterKitApplication {
 
     public static void main(String[] args) {
